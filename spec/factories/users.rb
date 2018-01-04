4cde67a44be3922a0f6634e8b38a85d72393bba5
Adam Prusse <adamprusse@Adams-MBP.ad.uvu.edu>
2018-01-04 14:10:33 -0700

    Implemented edit and new functionality

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
   factory :user do
     first_name 'Jon'
     last_name 'Snow'
-    email "test@test.com"
+    email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end
@@ -14,7 +14,7 @@ FactoryGirl.define do
   factory :admin_user, class: "AdminUser" do
     first_name 'Admin'
     last_name 'User'
-    email "admin@user.com"
+    email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end
