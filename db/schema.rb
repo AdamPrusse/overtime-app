dd4e32c43b1238000ae28f9bc0de1315ddb8d089
Adam <a_prusse@hotmail.com>
2018-01-11 10:29:53 -0700

    Implemented edit functionality for the status

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180109205123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
