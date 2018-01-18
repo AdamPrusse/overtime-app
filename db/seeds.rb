759673c2383e23dc0f70d2a959c2800f9b81ca22
Adam <a_prusse@hotmail.com>
2018-01-18 15:50:40 -0700

    Integreated validations and defaults for audit log attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 					password_confirmation: "asdfasdf", 
 					first_name: "Jon", 
 					last_name: "Snow",
-					phone: "13852360174")
+					phone: "3852360174")
 
 puts "1 User created"
 
@@ -12,12 +12,20 @@ AdminUser.create(email: "admin@test.com",
 				 password_confirmation: "asdfasdf", 
 				 first_name: "Admin", 
 				 last_name: "Name",
-				 phone: "13852360174")
+				 phone: "3852360174")
 
 puts "1 Admin User created"
+
+100.times do |audit_log|
+	AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 6.days))
+end
+
+puts "100 Audit Logs have been created"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
+
+
